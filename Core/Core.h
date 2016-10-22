89b0d4c22700ad333876d34d2bc19dd474f23f29
Marton <marton.kerekes@misys.com>
2016-10-22 18:24:18 +0300

    coredata added

---
+++ Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Kerekes Jozsef-Marton on 2016. 10. 20..
+//  Copyright © 2016. mkerekes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
